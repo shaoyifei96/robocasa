f677b9260e85cc1ec2ac57ecdda53d0ead5406b4
Carperis <samqiupeng@hotmail.com>
2025-05-01 03:46:56 -0400

    fix left door kind for StoreFruit

--- robocasa/environments/kitchen/multi_stage/customized_tasks/store_fruit.py
+++ robocasa/environments/kitchen/multi_stage/customized_tasks/store_fruit.py
@@ -22,8 +22,14 @@ class StoreFruit(Kitchen):
         The cabinet to place the fruit in and the counter to initialize it on.
         """
         super()._setup_kitchen_references()
-        self.cab = self.register_fixture_ref("cab", dict(id=FixtureType.DOOR_TOP_HINGE_SINGLE))
+        # self.cab = self.register_fixture_ref("cab", dict(id=FixtureType.DOOR_TOP_HINGE_SINGLE))
         # self.cab = self.register_fixture_ref("cab", dict(id=FixtureType.DOOR_TOP_HINGE_DOUBLE))
+        left_cabinet_names = [name for name in self.fixtures.keys() if "cab" in name.lower() and hasattr(self.fixtures[name], "orientation") and "left" in self.fixtures[name].orientation.lower()]
+        if left_cabinet_names:
+            self.cab = self.fixtures[left_cabinet_names[0]]
+        else:
+            self.cab = self.register_fixture_ref("cab", dict(id=FixtureType.CABINET_TOP))
+
         self.counter = self.register_fixture_ref("counter", dict(id=FixtureType.COUNTER, ref=self.cab))
         self.init_robot_base_pos = self.cab
 
